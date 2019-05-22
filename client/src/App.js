4d0f0d69aa756c72a19a9597d7a5b43dfe00fc1b
doctoron <ron.antonio@me.com>
2019-05-21 22:58:51 -0400

    Added concurrently npm

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import {
   BrowserRouter as Router,
   Route,
-  // Link,
   Switch,
-  // Redirect
 } from "react-router-dom";
 import MenuBar from './components/MenuBar';
 import Login from './components/LoginModal/Login';
 import PrivateRoute from './components/Auth/PrivateRoute';
 import vaccineList from './components/RenderVacs/vaccineList'
-// import PVRec2 from './Pages/PVRec';
 import Register from './components/Register';
-// import RoundAbout from './components/RoundAbout';
-// import { Jumbotron, Button, Alert, Fade } from 'reactstrap';
 
 import './App.css';
 
@@ -25,7 +20,6 @@ class App extends Component {
       isOpen: false,
       authenticated: false,
       private: false,
-      // redirect: false
     }
     this.connectToServer = this.connectToServer.bind(this);
     this.toggle = this.toggle.bind(this);
@@ -61,9 +55,8 @@ class App extends Component {
               <Route exact path="/" component={Login} />
               <Route exact path="/login" component={() => <Login history={this.props.history} showModal={this.state.isOpen} toggle={this.toggle} />} />
               <Route path="/register" component={Register} />
-              <Route path="/test" component={vaccineList} />
-              {/* <PrivateRoute path="/private" component={PVRec} /> */}
               <PrivateRoute path="/private" component={vaccineList} />
+              {/* <Route path="/test" component={vaccineList} /> */}
             </Switch>
 
           </div>
