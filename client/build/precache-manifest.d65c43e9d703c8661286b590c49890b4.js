0e338372040931551a703b8552135c0a04533ceb
doctoron <ron.antonio@me.com>
2019-06-16 06:32:34 -0400

    deployment

---
+++ client/build/precache-manifest.d65c43e9d703c8661286b590c49890b4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd8fdaa7b797d5d5be17",
+    "url": "/static/css/main.1be06a0a.chunk.css"
+  },
+  {
+    "revision": "dd8fdaa7b797d5d5be17",
+    "url": "/static/js/main.66f60e1a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "030d976793b45c52179f",
+    "url": "/static/css/2.acaff2e6.chunk.css"
+  },
+  {
+    "revision": "030d976793b45c52179f",
+    "url": "/static/js/2.fb914fdb.chunk.js"
+  },
+  {
+    "revision": "96c940a1e57f8e6b1682b95f34e08f54",
+    "url": "/static/media/VacTRACK-Logo.96c940a1.jpg"
+  },
+  {
+    "revision": "171fa90132cfaa00c59f4faa58aa37bf",
+    "url": "/static/media/puzzle3.171fa901.jpg"
+  },
+  {
+    "revision": "d5db1643f1a024d5d44a9aa892f7055b",
+    "url": "/static/media/adult-celebrate.d5db1643.jpg"
+  },
+  {
+    "revision": "270687815c2e349c12652276ce7c0d90",
+    "url": "/index.html"
+  }
+];
